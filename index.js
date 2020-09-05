bf7944b8df4879f1c5dca6df2831ab0161c5c931
Ankit Ghosh <ankitghosh311@outlook.com>
2020-09-05 21:01:03 +0530

    add friend suggestion routers and update README and fix CORS error

--- index.js
+++ index.js
@@ -23,6 +23,19 @@ const profileUpload = require("./uploadRouter/profileUpload");
 
 // middleware
 app.use(express.json())
+
+// CORS handle
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*")
+    res.header("Access-Control-Allow-Headers","*")
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods","PUT","POST","PATCH","DELETE","GET")
+        return res.status(200).json({});
+    }
+    next()
+})
+
+
 app.use(userRouter)
 app.use(otherImageUpload)
 app.use(profileUpload)
